821aa91e2b3edd03b39bb4772be3234c5278b7ba
Raymond Zou <raymond.1zou@gmail.com>
2026-07-22 17:20:46 -0700

    add interaction scripts

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -1,4 +1,4 @@
-extends Control
+extends CanvasLayer
 
 # NOTE: assume main.tscn is the parent
 @onready var day : Day = get_parent()
@@ -8,12 +8,20 @@ func _ready() -> void:
 	day.day_ended.connect(_on_day_ended)
 	%RestartButton.pressed.connect(_button_pressed)
 
+	var player := get_parent().get_node("Player")
+	player.focused_changed.connect(_on_focused_changed)
+
 func _button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
 
 func _on_hour_updated(_new_hour : int):
 	%HourLabel.text = day.get_readable_hour()
 
-# show a button that will reset the game
 func _on_day_ended():
 	%DayOverMenu.show()
+
+func _on_focused_changed(text: String, visible: bool) -> void:
+	print(text)
+	%InteractLabel.visible = visible
+	if visible:
+		%InteractLabel.text = "[E] " + text
