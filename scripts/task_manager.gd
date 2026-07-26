fd8285fc7c3e1dcac91ce095a0a926aa2ec4281e
Raymond Zou <raymond.1zou@gmail.com>
2026-07-26 00:47:30 -0700

    add: Lucky Strike upgrade

--- scripts/task_manager.gd
+++ scripts/task_manager.gd
@@ -7,6 +7,8 @@ signal all_tasks_completed
 @export var interactable_tasks : Array[InteractableTask] = [] # all the interactable tasks in the scene
 @export var curr_tasks : Array[Task] = []
 var is_tasks_completed : bool = false 
+const LUCKY_COUNT : int = 3 # maximum number of times Lucky Strike can be used
+var curr_lucky_count : int = 0
 
 func _ready() -> void:
 	for interactable : InteractableTask in interactable_tasks:
@@ -24,10 +26,29 @@ func is_incomplete(task: Task) -> bool:
 	return task.status == Task.Status.INCOMPLETE
 
 
+# filter the array for incomplete tasks
+# return the first item of the array
+func complete_random_task():
+	var incomplete_tasks : Array[Task] = curr_tasks.filter(func(t): return t.status == Task.Status.INCOMPLETE)
+	if not incomplete_tasks.is_empty():
+		var first : Task = incomplete_tasks[0]	
+		first.complete_task()		
+		print("Lucky Strike hits! %s has been completed!" % first.description)
+		curr_lucky_count += 1
+		tasks_updated.emit(curr_tasks)
+
 func _on_interactable_task_completed(_task: Task) -> void:
 	# if task in curr_tasks:
 	tasks_updated.emit(curr_tasks)
 
+	# check for Lucky Strike upgrade
+	# AND has not been activated more than LUCKY_COUNT times
+	var lucky : LevelUpgradeInfo = preload("res://upgrades/lucky_strike.tres")
+	if lucky in GameManager.chosen_upgrades:
+		var x = randi_range(0, 1)
+		if x == 0:
+			complete_random_task()
+	
 	# check if there are any incomplete tasks
 	var has_incomplete = curr_tasks.any(is_incomplete)
 	if not has_incomplete:
